dd4e4b45c5d6303a28a5d914e8d2426583e75781
ale.fabbian <fabbian.alessandro@gmail.com>
2023-07-12 18:58:47 +0200

    backup before closing session

--- app.js
+++ app.js
@@ -5,22 +5,19 @@ const cards = document.querySelectorAll('.card');
 
 function handleOnMouseMove (e) {
   
-  //DA PROVARE SE FUNZIONA COSI:
-  // const rect = e.getBoundingRect(),
-  // x = e.clientX - rect.left,
-  // y = e.clientY - rect.top;
-  
-  const {currentTarget: target} = e;
+ 
+  const {currentTarget: target} = e; //DEFINISCE LA VARIABILE TARGET COME VALORE DELLA PROPRIETà CURRENT TARGET DELL'OGGETTO e; E DA ALLA VARIABILE IL VALORE e. In sostanza mi permette di rimpiazzare e.target perché non sono all'interno di un evento, ma di una funzione che viene utilizzata in un evento
 
   const rect = target.getBoundingClientRect();
-  let x = e.clientX - rect.left;
+  let x = e.clientX - rect.left
   let y = e.clientY - rect.top;
 
   target.style.setProperty("--mouse-x", `${x}px`);
   target.style.setProperty("--mouse-y", `${y}px`)
-  console.log(x)
 }
 
 for (let card of cards) {
-    card.onmousemove = e => handleOnMouseMove(e);
+    setTimeout(()=>
+      {card.onmousemove = e => handleOnMouseMove(e)
+      }, 100);
   }
